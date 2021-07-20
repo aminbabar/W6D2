e9f29c0c6af0b4d136e764090967e4fb59dc385c
mordes89 <mordes89@gmail.com>
2021-07-20 14:28:00 -0400

    sorry im late

--- HW/meta_corgis.rb
+++ HW/meta_corgis.rb
@@ -107,14 +107,34 @@ class MetaCorgiSnacks
   def initialize(snack_box, box_id)
     @snack_box = snack_box
     @box_id = box_id
+
+    snack_box.methods.grep(/^get_(.*)_info$/) { MetaCorgiSnacks.define_snack $1 }
   end
 
-  def method_missing(name, *args)
-    # Your code goes here...
-  end
+  # def method_missing(name, *args)
+  #   # Your code goes here...
+  #   name_info = "get_#{name}_info"
+  #   name_tastiness = "get_#{name}_tastiness"
+
+  #   info = @snack_box.send(name_info, @box_id)
+  #   tastiness = @snack_box.send(name_tastiness, @box_id)
+  #   result = "#{name}: #{info}: #{tastiness} "
+  #   tastiness > 30 ? "* #{result}" : result
+
+  # end
 
 
   def self.define_snack(name)
     # Your code goes here...
+
+    define_method(name) do
+      name_info = "get_#{name}_info"
+      name_tastiness = "get_#{name}_tastiness"
+      info = @snack_box.send(name_info, @box_id)
+      tastiness = @snack_box.send(name_tastiness, @box_id)
+      result = "#{name}: #{info}: #{tastiness} "
+      tastiness > 30 ? "* #{result}" : result
+    end
+
   end
 end
