81c2a34f785d3e5c496703c4896a81b925b7005e
aminbabar <amin.babar1@gmail.com>
2021-07-20 17:47:00 -0400

    last commit for active record

--- active_record_1/lib/00_attr_accessor_object.rb
+++ active_record_1/lib/00_attr_accessor_object.rb
@@ -1,5 +1,56 @@
 class AttrAccessorObject
+
+
+  
+
   def self.my_attr_accessor(*names)
-    # ...
+
+
+    names.each do |name|
+      define_method(name) do
+        s = "@" + name.to_s
+        self.instance_variable_get(s)
+      end
+
+      arg1 = name.to_s + '='
+      define_method(arg1) do |arg|
+        s = "@" + name.to_s
+        self.instance_variable_set(s, arg)
+      end
+    end
+
   end
+
 end
+
+
+
+
+
+
+
+
+
+# class Example
+#   # attr_accessor :foo, :bar
+#   def initialize()
+#     @foo = "foo"
+#     @bar = "bar"
+#   end
+
+#   # def foo
+#   #   @foo
+#   # end
+
+#   # def foo=(value)
+#   #   @foo = value
+#   # end
+
+
+# end
+
+# # a = Example.new()
+# # p a.instance_variable_get(:@foo)
+
+
+
--- active_record_1/lib/01_sql_object.rb
+++ active_record_1/lib/01_sql_object.rb
@@ -1,3 +1,4 @@
+require "byebug"
 require_relative 'db_connection'
 require 'active_support/inflector'
 # NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
@@ -6,17 +7,39 @@ require 'active_support/inflector'
 class SQLObject
   def self.columns
     # ...
+    @eles ||= DBConnection.execute2(<<-SQL)
+    SELECT *
+    FROM "#{self.table_name}"
+    SQL
+
+    @eles[0].map(&:to_sym)
+
   end
 
   def self.finalize!
+    @attributes ||= {}
+    self.columns.each do |col|
+       @attributes[col] ||= true
+    end
+  
+    @attributes.each do |k, v|
+      define_method(k) do
+        self.attributes[k]
+      end
+      arg1 = k.to_s + '='
+      define_method(arg1) do |val|
+        self.attributes[k] = val
+      end
+
+    end
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    @table_name ||= self.to_s.tableize
   end
 
   def self.all
@@ -33,10 +56,19 @@ class SQLObject
 
   def initialize(params = {})
     # ...
+
+    params.each do |k, v|
+      # debugger
+      k = k.to_sym
+      # raise "unknown attribute '#{k}'" if key
+      self.send(k, v)
+    end
+
   end
 
   def attributes
     # ...
+    @attributes ||= {}
   end
 
   def attribute_values
@@ -55,3 +87,9 @@ class SQLObject
     # ...
   end
 end
+
+
+class BigDog < SQLObject
+end
+
+p BigDog.table_name # => "big_dogs"
